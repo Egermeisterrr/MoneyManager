09c30cdf465baa26b370bef0da9973eb68e00814
Egor Fokin <egor.fokin.3005@mail.ru>
2026-02-28 18:37:40 +0300

    fixed detekt and add prePushChecks

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,18 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.detekt) apply false
 }
+
+tasks.register("prePushChecks") {
+    group = "verification"
+    description = "Runs static analysis checks before git push."
+    dependsOn(
+        ":app:detekt",
+        ":data_expenses:detekt",
+        ":domain_expenses:detekt",
+        ":feature_expenses:detekt",
+        ":app:lintDebug",
+        ":data_expenses:lintDebug",
+        ":domain_expenses:lintDebug",
+        ":feature_expenses:lintDebug"
+    )
+}
